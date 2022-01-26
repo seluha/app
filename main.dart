ccb771f9555dcaaca7be72805c34be3c6085c88a
soliha shemsu <97150597+seluha@users.noreply.github.com>
2022-01-26 20:06:03 +0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,19 +1,64 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( Center(
-    child: Text(
-      "Hello world",textDirection: TextDirection.ltr,
-
-    style: TextStyle(fontStyle: FontStyle.italic,
-    fontWeight: FontWeight.bold,
-      backgroundColor: Colors.red[400],
-      fontSize: 50,
-    ),
-
-        ),
-
-  ));
+  runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello World',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.deepOrange,
+            ),
+            Row(children: [
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.lightBlue,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.lightGreen,
+              ),
+            ]),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.black,
+            ),
+            Row(children: [
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.lightGreenAccent,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.deepOrange,
+              ),
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.pink,
+              ),
+            ]),
+          ],
+        ),
+      ),
+    );
+  }
+}
